236059c2e1dcdc38a361e455e6cfbc2bea3dc598
Chintala-Keerthana <keerthiramana.44@gmail.com>
2026-07-09 22:41:59 +0530

    app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
-import Certifications from "./components/Certifications";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Loader from "./components/Loader";
